04dfe95572619acc33ba44a7ffec0b780f2aeaf4
k-l-lambda <k.l.lambda@gmail.com>
2020-04-09 09:27:03 +0800

    index.browser.ts created.

---
+++ index.browser.ts
@@ -0,0 +1,15 @@
+
+import * as staffSvg from "./inc/staffSvg";
+import * as staffNotation from "./inc/staffSvg/staffNotation";
+
+import SheetLive from "./app/components/sheet-live.vue";
+import SheetSigns from "./app/components/sheet-signs.vue";
+
+
+
+export {
+	staffSvg,
+	staffNotation,
+	SheetLive,
+	SheetSigns,
+};
