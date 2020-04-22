907ddd770e4b9b670ac4e2e03f41fc48eab1aa53
k-l-lambda <k.l.lambda@gmail.com>
2020-04-22 17:58:14 +0800

    lilyDocument.ts: parseRaw added.

--- inc/lilyParser/lilyDocument.ts
+++ inc/lilyParser/lilyDocument.ts
@@ -1,16 +1,34 @@
 
-const parseRaw = data => {
-	// TODO:
-	return data;
+class Block {
+	data: object;
+
+
+	constructor (data) {
+		this.data = data;
+	}
+};
+
+
+const parseRaw = (data, context) => {
+	switch (context) {
+	case "root":
+		return data.map(block => parseRaw(block, "toplevel_block"));
+
+	case "toplevel_block":
+		return new Block(data);
+
+	default:
+		return data;
+	}
 };
 
 
 
 export default class LilyDocument {
-	root: any;
+	root: object;
 
 
 	constructor (data) {
-		this.root = parseRaw(data);
+		this.root = parseRaw(data, "root");
 	}
 };
