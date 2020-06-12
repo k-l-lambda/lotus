b076b4cf8cef8b1579fc7f0a881bac6349a95602
k-l-lambda <k.l.lambda@gmail.com>
2020-06-12 17:33:11 +0800

    fixed test of bundleScore.ts

--- tests/bundleScore.ts
+++ tests/bundleScore.ts
@@ -3,6 +3,7 @@ import fs from "fs";
 
 import "../env.js";
 import {markScore} from "../backend/scoreMaker";
+import loadLilyParser from "../backend/loadLilyParserNode";
 import LogRecorder from "../inc/logRecorder";
 
 
@@ -10,8 +11,10 @@ import LogRecorder from "../inc/logRecorder";
 const main = async (...args) => {
 	const logger = new LogRecorder({enabled: true});
 
+	const lilyParser = await loadLilyParser();
+
 	const ly = fs.readFileSync(args[0]).toString();
-	const result = await markScore(ly, {logger});
+	const result = await markScore(ly, lilyParser, {logger});
 	console.log("result:", result, logger);
 
 	const outputPath = args[1];
