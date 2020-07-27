0dfeff37ce187a14e7393a8998af79e39a3b129e
k-l-lambda <k.l.lambda@gmail.com>
2020-07-27 10:34:20 +0800

    index.browser.ts: removed shims-vue.

--- index.browser.ts
+++ index.browser.ts
@@ -5,7 +5,6 @@ import {recoverJSON} from "./inc/jsonRecovery";
 import * as lilyParser from "./inc/lilyParser";
 import LogRecorder from "./inc/logRecorder";
 
-import "./app/shims-vue";
 import SheetLive from "./app/components/sheet-live.vue";
 import SheetSigns from "./app/components/sheet-signs.vue";
 import * as SheetBaker from "./app/sheetBaker";
