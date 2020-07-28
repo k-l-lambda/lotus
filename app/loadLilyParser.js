c7fdd138b71194b7a4bbebe8cad1cadea8f6add1
k-l-lambda <k.l.lambda@gmail.com>
2020-07-28 18:07:52 +0800

    loadLilyParser.js: parser loading cost profile added.

--- app/loadLilyParser.js
+++ app/loadLilyParser.js
@@ -9,9 +9,13 @@ let parser = null;
 
 export default async function load () {
 	if (!parser) {
+		const t0 = performance.now();
+
 		const {default: grammarURL} = await import("../inc/lilyParser/lilypond.jison");
 		const grammar = await (await fetch(grammarURL)).text();
 		//console.log("grammar:", grammar);
+
+		const t1 = performance.now();
 
 		// mute jison logs during grammar loading
 		lilyParser.hookJisonPrint();
@@ -21,6 +25,9 @@ export default async function load () {
 			console.error("lily parser create failed.");
 
 		lilyParser.hookJisonPrint((...args) => console.log("[JISON]", ...args));
+
+		const t2 = performance.now();
+		console.debug("lilyParser loading cost:", t1 - t0, t2 - t1);
 	}
 
 	return parser;
