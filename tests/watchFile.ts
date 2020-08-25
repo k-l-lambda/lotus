18c2fcc27e6eaf339d0e3fcb9b3b245839ccbccc
k-l-lambda <k.l.lambda@gmail.com>
2020-08-25 10:13:30 +0800

    tests/watchFile.ts: file content sync added.

--- tests/watchFile.ts
+++ tests/watchFile.ts
@@ -1,11 +1,38 @@
 
 import fs from "fs";
+import * as diff from "diff";
 
+
+
+const target = {
+	text: null,
+
+	init (value) {
+		this.text = value;
+		console.log("target:", this.text);
+	},
+
+	increment (patch) {
+		this.text = diff.applyPatch(this.text, patch);
+		console.log("target:", this.text);
+	},
+};
 
 
 const main = filename => {
+	let oldContent = fs.readFileSync(filename).toString();
+	target.init(oldContent);
+
 	fs.watchFile(filename, (curr, prev) => {
-		console.log("changed:", prev.mtime, curr.mtime);
+		console.debug("changed:", prev.mtime, curr.mtime);
+
+		const content = fs.readFileSync(filename).toString();
+		//const differ = diff.diffChars(oldContent, content);
+		const patch = diff.createPatch(filename, oldContent, content);
+		//console.log("patch:", patch);
+		target.increment(patch);
+
+		oldContent = content;
 	});
 };
 
