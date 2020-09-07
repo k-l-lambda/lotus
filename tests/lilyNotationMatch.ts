8783687e198e20c64460bdf337f126916bfe9f31
k-l-lambda <k.l.lambda@gmail.com>
2020-09-07 15:22:46 +0800

    lilyNotationMatch.ts: dump offset map.

--- tests/lilyNotationMatch.ts
+++ tests/lilyNotationMatch.ts
@@ -90,6 +90,7 @@ const checkFile = async filename => {
 		omitS,
 		coverage,
 		averageTickOffset,
+		offsetMap,
 	};
 };
 
@@ -119,8 +120,8 @@ const main = async () => {
 	for (const lyFile of lyFiles) {
 		try {
 			const result = await checkFile(lyFile);
-			if (argv.breakOnLargeOffset && Math.abs(result.averageTickOffset) > 1) {
-				console.warn("Large averageTickOffset!");
+			if (argv.breakOnLargeOffset && Math.abs(result.averageTickOffset) > 16) {
+				console.warn("Large averageTickOffset:", result.offsetMap);
 				break;
 			}
 			if (argv.dumpOnError && (result.omitC + result.omitS > 0 || result.averageTickOffset)) {
