4e3f9cd0add4484d9dfed05fde164f94e463388d
k-l-lambda <k.l.lambda@gmail.com>
2020-09-15 10:39:41 +0800

    sheet-token.vue: added data-track on DOM.

--- app/components/sheet-token.vue
+++ app/components/sheet-token.vue
@@ -3,6 +3,7 @@
 		:transform="`translate(${token.x}, ${token.y})`"
 		:class="classes"
 		:data-href="token.href"
+		:data-track="token.track"
 		@click="$emit('click', $event)"
 	>
 		<use :class="token.classes" :data-href="token.href" :xlink:href="`#sign-${token.hash}`" />
