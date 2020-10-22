7a9c71fff1d38b5fb8573cec490395f635569799
k-l-lambda <k.l.lambda@gmail.com>
2020-10-22 17:24:50 +0800

    Added env variable of MOBILE_MODE.

--- backend/canvas.ts
+++ backend/canvas.ts
@@ -1,11 +1,15 @@
 
-import {loadImage, createCanvas} from "canvas";
-// eslint-disable-next-line
+//import {loadImage, createCanvas} from "canvas";
 import {PNGStream} from "canvas";
+
+const {loadImage, createCanvas}: any = process.env.MOBILE_MODE ? require("canvas") : {};
 
 
 
 const svgToPng = async (sourceURL: string|Buffer): Promise<PNGStream> => {
+	if (process.env.MOBILE_MODE)
+		return;
+
 	const image = await loadImage(sourceURL);
 
 	const canvas = createCanvas(image.width, image.height);
